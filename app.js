0a30f6c6e119c8464998757a88f2b67bae15153c
sinhey <sinheyyy@gmail.com>
2024-05-25 01:28:31 +0900

    update port

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require("./routes/index");
+require('dotenv').config()
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-app`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
@@ -20,6 +23,6 @@ mongoose
         console.log("DB connection fail", err);
     });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server on 5000");
 });
